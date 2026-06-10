be4248d4cb78833a7c79df65bd723974f2b96ae8
Arturo <arthur0232002@gmail.com>
2026-06-10 17:10:02 -0600

    Fix: Roles de administrador y cliente agregados

--- app.py
+++ app.py
@@ -13,41 +13,51 @@ playlist_negocio.agregar_cancion(Cancion(2, "Bohemian Rhapsody", "Queen"))
 playlist_negocio.agregar_cancion(Cancion(3, "Shape of You", "Ed Sheeran"))
 
 @app.route('/')
-def index():
-   
+@app.route('/admin')
+def admin():
     canciones_array = []
     actual = playlist_negocio.cabeza
     while actual is not None:
         canciones_array.append({
-            'titulo': actual.cancion.titulo,
-            'artista': actual.cancion.artista,
+            'titulo': actual.cancion.titulo, 'artista': actual.cancion.artista,
             'es_actual': (actual == playlist_negocio.actual)
         })
         actual = actual.siguiente
-        
+    txt_sonando = f"{playlist_negocio.actual.cancion.titulo} - {playlist_negocio.actual.cancion.artista}" if playlist_negocio.actual else "Ninguna"
     
-    if playlist_negocio.actual:
-        txt_sonando = f"{playlist_negocio.actual.cancion.titulo} - {playlist_negocio.actual.cancion.artista}"
-    else:
-        txt_sonando = "Ninguna (Playlist vacía)"
+    
+    return render_template('index.html', canciones=canciones_array, cancion_sonando=txt_sonando, es_admin=True)
 
-
-    return render_template('index.html', canciones=canciones_array, cancion_sonando=txt_sonando)
+@app.route('/')
+@app.route('/cliente')
+def cliente():
+    canciones_array = []
+    actual = playlist_negocio.cabeza
+    while actual is not None:
+        canciones_array.append({
+            'titulo': actual.cancion.titulo, 'artista': actual.cancion.artista,
+            'es_actual': (actual == playlist_negocio.actual)
+        })
+        actual = actual.siguiente
+    txt_sonando = f"{playlist_negocio.actual.cancion.titulo} - {playlist_negocio.actual.cancion.artista}" if playlist_negocio.actual else "Ninguna"
+    
+    
+    return render_template('index.html', canciones=canciones_array, cancion_sonando=txt_sonando, es_admin=False)
 
 @app.route('/siguiente')
 def siguiente():
     playlist_negocio.avanzar_cancion()
-    return redirect(url_for('index'))
+    return redirect(url_for('admin'))
 
 @app.route('/anterior')
 def anterior():
     playlist_negocio.retroceder_cancion()
-    return redirect(url_for('index'))
+    return redirect(url_for('admin'))
 
 @app.route('/eliminar')
 def eliminar():
     playlist_negocio.eliminar_actual()
-    return redirect(url_for('index'))
+    return redirect(url_for('admin'))
 
 @app.route('/agregar', methods=['POST'])
 def agregar():
@@ -57,7 +67,7 @@ def agregar():
     
     
     playlist_negocio.agregar_cancion(Cancion(nuevo_id, titulo, artista))
-    return redirect(url_for('index'))
+    return redirect(url_for('admin'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
